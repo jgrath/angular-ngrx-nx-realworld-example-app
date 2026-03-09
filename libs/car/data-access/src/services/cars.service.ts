741876d33f7625c857fb2fc88bbe782b3b227c87
STEFAN\s_gra <stefan.grath@gmail.com>
2026-03-09 14:46:22 +0000

    added CarApiService

--- libs/car/data-access/src/services/cars.service.ts
+++ libs/car/data-access/src/services/cars.service.ts
@@ -1,12 +1,13 @@
 import { inject, Injectable } from '@angular/core';
 import { ApiService } from '../../../../core/http-client/src';
+import { CarApiService } from '../../../../core/http-client/src/lib/car.api.service';
 import { Observable, of } from 'rxjs';
 import { Article } from '../../../../core/api-types/src';
 import { Car } from '../../../../core/api-types/src/lib/car';
 
 @Injectable({ providedIn: 'root' })
 export class CarsService {
-  private readonly apiService = inject(ApiService);
+  private readonly carApiService = inject(CarApiService);
 
   CAR_DATA: Car[] = [
     { id: 1, brand: 'Tesla', model: 'Model 3', serviceDate: '2026-03-19T13:23' },
@@ -20,8 +21,8 @@ export class CarsService {
   ];
 
   findAllCars(): Observable<{ cars: Car[] }> {
-    return of({ cars: this.CAR_DATA });
+    //return of({ cars: this.CAR_DATA });
     // call rest endpoint here:
-    //return this.apiService.get('/cars');
+    return this.carApiService.get('/cars');
   }
 }
---
+++ libs/core/http-client/src/lib/car.api.service.ts
@@ -0,0 +1,26 @@
+import { Inject, Injectable, inject } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class CarApiService {
+  private readonly http = inject(HttpClient);
+  private readonly car_api_url = 'http://localhost:5014/api';
+
+  get<T>(url: string, params: HttpParams = new HttpParams()): Observable<T> {
+    return this.http.get<T>(`${this.car_api_url}${url}`, {
+      headers: this.headers,
+      params,
+      withCredentials: true,
+    });
+  }
+
+  get headers(): HttpHeaders {
+    const headersConfig = {
+      'Content-Type': 'application/json,',
+      Accept: 'application/json',
+    };
+
+    return new HttpHeaders(headersConfig);
+  }
+}
